bde14b0c20615975f1c9b3c257097f78347eaca7
Matheus Goncalves <matheusgoncalves@MacBook-Air-de-Matheus.local>
2019-11-05 13:27:37 -0300

    notifications with mongoDB

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmenController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,9 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointment', AppointmenController.store);
+routes.get('/appointment', AppointmenController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
