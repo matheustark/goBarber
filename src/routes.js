625cfe3cb338106e8dba8430c117b2e18ace21aa
Matheus Goncalves <matheusgoncalves@MacBook-Air-de-Matheus.local>
2019-11-05 17:05:22 -0300

    cancel appointment

--- src/routes.js
+++ src/routes.js
@@ -26,10 +26,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointment', AppointmenController.store);
 routes.get('/appointment', AppointmenController.index);
+routes.delete('/appointment/:id', AppointmenController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
